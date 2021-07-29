c0456aa86eb9905d4730f2eb8e468d5e1e919f05
Stephen <stephenjeffreywhite@outlook.com>
2021-07-29 02:01:58 -0400

    Add project files.

---
+++ BlazorLiquidSeperation/Models/BingImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorLiquidSeperation.Models
+{
+    public class BingImage
+    {
+
+        public BingImage(string pageUrl, string thumbUrl, string title, string copyRight, string date, string fullUrl)
+        {
+            PageUrl = pageUrl;
+            ThumbUrl = thumbUrl;
+            Title = title;
+            CopyRight = copyRight;
+            Date = date;
+            FullUrl = fullUrl;
+        }
+
+        [JsonPropertyName("pageUrl")]
+        public string PageUrl { get; set;}
+        [JsonPropertyName("thumbUrl")]
+        public string ThumbUrl { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("copyright")]
+        public string CopyRight { get; set; }
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+        [JsonPropertyName("fullUrl")]
+        public string FullUrl { get; set; }
+    }
+}
