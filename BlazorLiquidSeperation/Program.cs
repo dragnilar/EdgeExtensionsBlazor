c49e6c7c5f1e416e901052ead3843cce716c374c
Stevie White <stephenjeffreywhite@outlook.com>
2021-08-06 11:57:35 -0400

    Removed DevExpress and using vanilla bootstrap instead; DX is hardcoded to use _content folder which is a show stopper.

--- BlazorLiquidSeperation/Program.cs
+++ BlazorLiquidSeperation/Program.cs
@@ -16,7 +16,6 @@ namespace BlazorLiquidSeperation
             // workaround to use JavaScript fetch to bypass url validation
             // see: https://github.com/dotnet/runtime/issues/52836
             builder.Services.AddScoped<HttpClient>(sp => new JsHttpClient(sp) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddDevExpressBlazor();
             builder.Services.AddBrowserExtensionServices(options =>
             {
                 options.ProjectNamespace = typeof(Program).Namespace;
