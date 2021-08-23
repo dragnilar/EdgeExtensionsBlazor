e767762159a824dcf8774d6c2becc28ea66fcd93
Stephen <stephenjeffreywhite@outlook.com>
2021-08-23 00:44:47 -0400

    Added museum card functionality

---
+++ BlazorLiquidSeperation/wwwroot/js/jsInteropScripts.js
@@ -0,0 +1,25 @@
+﻿window.setBackgroundImage1 = (url) => document.getElementsByTagName("body")[0].style =
+    `background-image: url(${url})`;
+
+window.runSearchQuery = (url) => chrome.search.query({text: url}, () => {
+    console.log(url);
+});
+
+function enableToasts() {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new window.bootstrap.Toast(toastEl, option);
+    });
+}
+
+function showSaveChangesToast() {
+    var toastTrigger = document.getElementById('saveChangesBtn');
+    var toastLiveExample = document.getElementById('saveChangesToast');
+    if (toastTrigger) {
+        toastTrigger.addEventListener('click', function () {
+            var toast = new bootstrap.Toast(toastLiveExample);
+
+            toast.show();
+        });
+    }
+}
