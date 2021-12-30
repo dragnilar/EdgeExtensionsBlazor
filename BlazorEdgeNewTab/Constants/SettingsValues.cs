23c0356c7613a5b58ea843dd1f992848b3f9e116
Stevie White <stephenjeffreywhite@outlook.com>
2021-12-30 00:21:47 -0500

    Remove unused settings

--- BlazorEdgeNewTab/Constants/SettingsValues.cs
+++ BlazorEdgeNewTab/Constants/SettingsValues.cs
@@ -12,9 +12,7 @@ public static class SettingsValues
     public const string QuickLinkBookMarkFolder   = "QuickLinkBookmarkFolder";
     public const string DefaultBookmarkFolderName = "Edge Quick Links";
     public const string ImageOfTheDayCache        = "ImageOfTheDayCache";
-    public const string ImageArchiveCache         = "ImageArchiveCache";
     public const string ReQueryImagesAfterTime    = "ReQueryImagesAfterTime";
-    public const string ReQueryArchiveAfterTime   = "ReQueryArchiveAfterTime";
     public const string UseQuickLinksFilter       = "UseQuickLinksFilter";
 
     public static DateTime DefaultRequeryDateTime =
