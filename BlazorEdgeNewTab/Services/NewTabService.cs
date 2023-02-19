2fab52007f222f39fc919666e9ba03f8e8091f21
Stevie White <stephenjeffreywhite@outlook.com>
2023-02-19 02:46:46 -0500

    Upgraded to Manifest v3, rewrote favicon aquisition to stay in compliance

--- BlazorEdgeNewTab/Services/NewTabService.cs
+++ BlazorEdgeNewTab/Services/NewTabService.cs
@@ -4,8 +4,8 @@ using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using BlazorEdgeNewTab.Constants;
 using BlazorEdgeNewTab.Models;
@@ -17,13 +17,14 @@ namespace BlazorEdgeNewTab.Services;
 
 public class NewTabService : INewTabService
 {
-    private HttpClient _httpClient;
     private readonly string BingImageOfDayUri =
         "https://api.allorigins.win/get?url=https%3A//www.bing.com/HPImageArchive.aspx%3Fformat%3Djs%26idx%3D0%26n%3D7";
 
+    private HttpClient _httpClient;
+
     public async Task<BingImageOfTheDayDto> GetImageOfDayDto()
     {
-        BingImageOfTheDayDto dto = new BingImageOfTheDayDto();
+        var dto = new BingImageOfTheDayDto();
         var reQueryTime = Convert.ToDateTime(Settings.GetSettingValue(SettingsValues.ReQueryImagesAfterTime)
                                              ?? DateTime.Now.AddDays(-1).ToString(CultureInfo.InvariantCulture));
         Console.WriteLine($"Re-Query Date for Bing Image Of Day In Cache Is: {reQueryTime.Date}");
@@ -37,36 +38,33 @@ public class NewTabService : INewTabService
             if (responseDto != null)
             {
                 var correctedJson = responseDto.contents.Replace("\\", "");
-                dto = JsonSerializer.Deserialize<BingImageOfTheDayDto>(correctedJson, new JsonSerializerOptions
-                {
-                    
-                });
+                dto = JsonSerializer.Deserialize<BingImageOfTheDayDto>(correctedJson, new JsonSerializerOptions());
                 Console.WriteLine($"Dto from bing: {dto.images[0].fullstartdate}");
-                var imageDate = DateTime.ParseExact(dto.images[0].fullstartdate, "yyyyMMddHHmm", CultureInfo.InvariantCulture);
+                var imageDate = DateTime.ParseExact(dto.images[0].fullstartdate, "yyyyMMddHHmm",
+                    CultureInfo.InvariantCulture);
                 Settings.UpdateSetting(SettingsValues.ImageOfTheDayCache, JsonSerializer.Serialize(dto));
                 Settings.UpdateSetting(SettingsValues.ReQueryImagesAfterTime,
                     imageDate.ToString(CultureInfo.InvariantCulture));
                 await Settings.SaveAsync();
                 return dto;
             }
-            else
-            {
-                Console.WriteLine($"Received no or a null response back from All Origins. :(");
-            }
+
+            Console.WriteLine("Received no or a null response back from All Origins. :(");
         }
         else
         {
-            Console.WriteLine($"Using cached Bing Image Of Day.");
+            Console.WriteLine("Using cached Bing Image Of Day.");
             dto = JsonSerializer.Deserialize<BingImageOfTheDayDto>(
                 Settings.GetSettingValue(SettingsValues.ImageOfTheDayCache));
         }
-        
+
         return dto;
     }
 
     public async Task<List<QuickLink>> SetUpQuickLinks(IWebExtensionsApi webExtensions)
     {
-        var quickLinks = new List<QuickLink>();
+        var quickLinks  = new List<QuickLink>();
+        var extensionId = await webExtensions.Runtime.GetId();
         var bookMarkNode =
             await webExtensions.Bookmarks.Search(Settings.GetSettingValue(SettingsValues.QuickLinkBookMarkFolder));
         var bookmarkTreeNodes = bookMarkNode.ToList();
@@ -82,14 +80,20 @@ public class NewTabService : INewTabService
             quickLinks.AddRange(quickLinkBookMarks.Select(quickLinkBookMark => new QuickLink
             {
                 QuickLinkTitle = quickLinkBookMark.Title,
-                QuickLinkImageUrl = "chrome://favicon/size/64/" + quickLinkBookMark.Url,
+                QuickLinkImageUrl = GetFaviconUrl(extensionId, quickLinkBookMark.Url),
                 QuickLinkUrl = quickLinkBookMark.Url,
                 QuickLinkId = quickLinkBookMark.Id,
                 Visible = true
             }));
         }
+
         return quickLinks;
     }
 
-    
+    private string GetFaviconUrl(string extensionId, string quickLinkUrl)
+    {
+        var encodedQuickLinkUrl = UrlEncoder.Default.Encode(quickLinkUrl);
+        var url                 = $"chrome-extension://{extensionId}/_favicon/?pageUrl={encodedQuickLinkUrl}&size=64";
+        return url;
+    }
 }
